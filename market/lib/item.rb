3bf268a402a915a506ebb6d152741f0a1d08b943
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-17 23:14:13 -0600

    Item has name and price

--- market/lib/item.rb
+++ market/lib/item.rb
@@ -1,5 +1,7 @@
 class Item
-
-  def initialize(attributes); end
-
+  attr_reader :name, :price
+  def initialize(attributes)
+    @name = attributes[:name]
+    @price = attributes[:price].delete("$,").to_f
+  end
 end
