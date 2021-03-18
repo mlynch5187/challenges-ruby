4ebf573aee719baf022999181c6f9630bdac3585
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-18 08:45:22 -0600

    Vendors can be added

--- market/test/market_test.rb
+++ market/test/market_test.rb
@@ -5,7 +5,6 @@ require './market/lib/market'
 
 class MarketTest < Minitest::Test
   def setup
-    skip
     @market = Market.new("South Pearl Street Farmers Market")
     @vendor1 = Vendor.new("Rocky Mountain Fresh")
     @vendor2 = Vendor.new("Ba-Nom-a-Nom")
@@ -14,14 +13,14 @@ class MarketTest < Minitest::Test
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-    @vendor1.stock(@item1, 35)
-    @vendor1.stock(@item2, 7)
-    @vendor2.stock(@item4, 50)
-    @vendor2.stock(@item3, 25)
-    @vendor3.stock(@item1, 65)
-    @market.add_vendor(@vendor1)
-    @market.add_vendor(@vendor2)
-    @market.add_vendor(@vendor3)
+    # @vendor1.stock(@item1, 35)
+    # @vendor1.stock(@item2, 7)
+    # @vendor2.stock(@item4, 50)
+    # @vendor2.stock(@item3, 25)
+    # @vendor3.stock(@item1, 65)
+    # @market.add_vendor(@vendor1)
+    # @market.add_vendor(@vendor2)
+    # @market.add_vendor(@vendor3)
   end
 
   def test_it_exists
@@ -36,23 +35,48 @@ class MarketTest < Minitest::Test
     assert_equal [], @market.vendors
   end
 
-  def test_it_has_vendors
-    skip
+  def test_vendors_can_be_added
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
     assert_equal [@vendor1, @vendor2, @vendor3], @market.vendors
   end
 
   def test_it_has_vendor_names
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
     skip
     assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @market.vendor_names
   end
 
   def test_it_has_vendors_that_sell
     skip
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+
     assert_equal [@vendor1, @vendor3], @market.vendors_that_sell(@item1)
     assert_equal [@vendor2], @market.vendors_that_sell(@item4)
   end
 
-  def test_it_haS_potential_revenue
+  def test_it_has_potential_revenue
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+
     skip
     assert_equal 29.75, @vendor1.potential_revenue
     assert_equal 345.00, @vendor2.potential_revenue
