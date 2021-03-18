7f2de9a1e20ca10bce76a4b04149ff7329d4076d
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-17 23:34:47 -0600

    Refactor method

--- market/lib/vendor.rb
+++ market/lib/vendor.rb
@@ -2,14 +2,10 @@ class Vendor
   attr_reader :name, :inventory
   def initialize(name)
     @name = name
-    @inventory = {}
+    @inventory = Hash.new(0)
   end
 
   def check_stock(item)
-    if @inventory[item] === nil
-      0
-    else
-      @inventory[item]
-    end
+    @inventory[item]
   end
 end
