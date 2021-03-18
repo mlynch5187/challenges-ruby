228b73ac4a413d6b2106bd85a49412335ea888b6
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-17 23:26:07 -0600

    Stock can be checked

--- market/lib/vendor.rb
+++ market/lib/vendor.rb
@@ -1,6 +1,15 @@
 class Vendor
-
+  attr_reader :name, :inventory
   def initialize(name)
+    @name = name
+    @inventory = {}
+  end
 
+  def check_stock(item)
+    if @inventory[item] === nil
+      0
+    else
+      @inventory[item]
+    end
   end
 end
