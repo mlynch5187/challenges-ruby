39b6d796fafcc29e8c5a82284e4ee5ab51f7df87
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-25 13:56:35 -0600

    Players can be eliminated

--- world_cup/lib/team.rb
+++ world_cup/lib/team.rb
@@ -1,6 +1,11 @@
 class Team
-  attr_reader :country
+  attr_reader :country, :eliminated
   def initialize(country)
     @country = country
+    @eliminated = false
+  end
+
+  def eliminated?
+    @eliminated
   end
 end
