01f8f2cb23ceffbc8233c000b8c29e8407cddb9c
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-27 07:22:31 -0600

    Write next round of museum tests

--- museum/test/museum_test.rb
+++ museum/test/museum_test.rb
@@ -12,9 +12,11 @@ class MuseumTest < Minitest::Test
     @imax = Exhibit.new({name: "IMAX",cost: 15})
     @patron_1 = Patron.new("Bob", 20)
     @patron_2 = Patron.new("Sally", 20)
+    @patron_3 = Patron.new("Johnny", 5)
     @patron_1.add_interest("Dead Sea Scrolls")
     @patron_1.add_interest("Gems and Minerals")
     @patron_2.add_interest("IMAX")
+    @patron_3.add_interest("Dead Sea Scrolls")
   end
 
   def test_it_exists
@@ -41,8 +43,50 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@gems_and_minerals)
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
-    
+
     assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recommend_exhibits(@patron_1)
     assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
   end
+
+  def test_patrons_can_be_admitted
+    assert_equal [], @dmns.patrons
+
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+
+    assert_equal [@patron_1, @patron_2, @patron_3], @dmns.patrons
+  end
+
+  def test_patrons_by_exhibit_interest
+    {
+      @gems_and_minerals => [@patron_1]
+      @dead_sea_scrolls => [@patron_1, @patron_3]
+      @imax => [@patron_2]
+    }
+    assert_equal expected, @dmns.patrons_by_exhibit_interest
+  end
 end
+
+
+# pry(main)> dmns.ticket_lottery_contestants(dead_sea_scrolls)
+# # => [#<Patron:0x00007fb2011455b8...>, #<Patron:0x6666fb20114megan...>]
+#
+# pry(main)> dmns.draw_lottery_winner(dead_sea_scrolls)
+# # => "Johnny" or "Bob" can be returned here. Fun!
+#
+# pry(main)> dmns.draw_lottery_winner(gems_and_minerals)
+# # => nil
+#
+# #If no contestants are elgible for the lottery, nil is returned.
+#
+# pry(main)> dmns.announce_lottery_winner(dead_sea_scrolls)
+# # => "Bob has won the Dead Sea Scrolls exhibit lottery"
+#
+# # The above string should match exactly, you will need to stub the return of `draw_lottery_winner` as the above method should depend on the return value of `draw_lottery_winner`.
+#
+# pry(main)> dmns.announce_lottery_winner(gems_and_minerals)
+# # => "No winners for this lottery"
+#
+# # If there are no contestants, there are no winners.
+# ```
