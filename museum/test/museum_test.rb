4541216b707edbd14adbde873b15da5a12f32690
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-27 07:04:30 -0600

    Write Museum test

---
+++ museum/test/museum_test.rb
@@ -0,0 +1,44 @@
+require 'minitest/autorun'
+require './museum/lib/exhibit'
+require './museum/lib/patron'
+require './museum/lib/museum'
+
+class MuseumTest < Minitest::Test
+
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+  end
+
+  def test_it_exists
+    assert_instance_of Museum, @dmns
+  end
+
+  def test_it_has_a_name
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+  end
+
+  def test_it_has_exhibits
+    assert_equal [], @dmns.exhibits
+  end
+
+  def test_exhibits_can_be_added
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
+  end
+
+  def test_it_has_recommended_exhibits
+    assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recommend_exhibits(@patron_1)
+    assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
+  end
+end
