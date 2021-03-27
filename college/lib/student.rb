142a8908220c623768c91daae9b03d33734f9267
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-27 06:30:13 -0600

    Pass first tests

--- college/lib/student.rb
+++ college/lib/student.rb
@@ -1,7 +1,12 @@
 class Student
-  attr_reader :name, :age
+  attr_reader :name, :age, :scores
   def initialize(attributes)
     @name = attributes[:name]
     @age = attributes[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
   end
 end
