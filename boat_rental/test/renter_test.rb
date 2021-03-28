9af662f444836affe79d0e06f967483db7f09f73
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-28 14:13:11 -0600

    Create renter test

---
+++ boat_rental/test/renter_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require './boat_rental/lib/renter'
+
+class RenterTest < Minitest::Test
+
+  def setup
+    @renter = Renter.new("Patrick Star", "4242424242424242")
+  end
+
+  def test_it_exists
+    assert_instance_of Renter, @renter
+  end
+
+  def test_it_has_a_name
+    assert_equal "Patrick Star", @renter.name
+  end
+
+  def test_it_has_a_card_number
+    assert_equal "4242424242424242", @renter.credit_card_number
+  end
+end
