f6ecedbc55f457400f84299dd36a5e14f706a310
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-29 03:48:10 -0600

    Boats can be charged

--- boat_rental/lib/dock.rb
+++ boat_rental/lib/dock.rb
@@ -9,4 +9,11 @@ class Dock
   def rent(boat, renter)
     rental_log[boat] = renter
   end
+
+  def charge(boat)
+    charges = {}
+    charges[:card_number] = @rental_log[boat].credit_card_number
+    charges[:amount] = boat.hours_rented * boat.price_per_hour
+    charges
+  end
 end
