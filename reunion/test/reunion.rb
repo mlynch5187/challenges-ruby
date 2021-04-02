8b0b7d49948ecf1205edcd441ee25c70989d8473
Michael Alex Lynch <mlynch5187@gmail.com>
2021-04-02 09:59:12 -0600

    Write reunion test

---
+++ reunion/test/reunion.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require './reunion/lib/activity'
+require './reunion/lib/reunion'
+
+def ReunionTest < Minitest::Test
+
+  def setup
+    @reunion = Reunion.new("1406 BE")
+    @activity_1 = Activity.new("Brunch")
+  end
+
+  def test_it_exists
+    assert_instance_of Reunion, @reunion
+  end
+
+  def test_it_has_a_name
+    assert_equal "1406 BE", @reunion.name
+  end
+
+  def test_it_has_activities
+    assert_equal [], @Reunion.activities
+
+    @reunion.add_activity(@activity_1)
+
+    assert_equal [@activity_1], @reunion.activities
+  end
+end
