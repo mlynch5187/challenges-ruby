67ba37b9066d738ff5e11d0382d55edb88942099
Michael Alex Lynch <mlynch5187@gmail.com>
2021-04-02 09:56:20 -0600

    Write next part of activity test

--- reunion/test/activity_test.rb
+++ reunion/test/activity_test.rb
@@ -34,4 +34,12 @@ class ActivityTest < Minitest::Test
     assert_equal ({"Maria" => 20, "Luther" => 40}), @activity.participants
     assert_equal 60, @activity.total_cost
   end
+
+  def test_it_has_amount_owed
+    @activity.add_participant("Maria", 20)
+    @activity.add_participant("Luther", 40)
+    assert_equal 60, @activity.total_cost
+    assert_equal 30, @activity.split
+    assert_equal ({"Maria" => 10, "Luther" => -10}), @activity.owed
+  end
 end
