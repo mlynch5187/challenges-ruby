cecb902114160487ab4e52993ff0c69477838a56
Michael Alex Lynch <mlynch5187@gmail.com>
2021-04-06 08:10:37 -0600

    Lottery has winners, and contestants

--- lottery/lib/colorado_lottery.rb
+++ lottery/lib/colorado_lottery.rb
@@ -1,5 +1,8 @@
 class ColoradoLottery
+  attr_reader :winners, :registered_contestants, :current_contestants
   def initialize()
-
+    @winners = []
+    @registered_contestants = {}
+    @current_contestants = {}
   end
 end
