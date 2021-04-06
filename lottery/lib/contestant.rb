0a052bc6bbaef52ce9c7418e7450c5ec7d399aaf
Michael Alex Lynch <mlynch5187@gmail.com>
2021-04-06 07:52:04 -0600

    Create out of state method

--- lottery/lib/contestant.rb
+++ lottery/lib/contestant.rb
@@ -6,4 +6,12 @@ class Contestant
     @state_of_residence = attributes[:state_of_residence]
     @spending_money = attributes[:spending_money]
   end
+
+  def out_of_state
+    if @state_of_residence != "CO"
+      true
+    else
+      false
+    end
+  end
 end
