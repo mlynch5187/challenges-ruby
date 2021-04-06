594e4e6c1ab54433ad112ef470f2c3889548ecf7
Michael Alex Lynch <mlynch5187@gmail.com>
2021-04-06 07:53:07 -0600

    Pass contetant tests

--- lottery/lib/contestant.rb
+++ lottery/lib/contestant.rb
@@ -1,10 +1,11 @@
 class Contestant
-  attr_reader :full_name, :age, :state_of_residence, :spending_money
+  attr_reader :full_name, :age, :state_of_residence, :spending_money, :game_interests
   def initialize(attributes)
     @full_name = attributes[:first_name] + " " + attributes[:last_name]
     @age = attributes[:age]
     @state_of_residence = attributes[:state_of_residence]
     @spending_money = attributes[:spending_money]
+    @game_interests = []
   end
 
   def out_of_state
@@ -13,5 +14,9 @@ class Contestant
     else
       false
     end
+  end
+
+  def add_game_interest(interest)
+    @game_interests << interest
   end
 end
