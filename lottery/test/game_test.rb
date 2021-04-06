edf6dd8d5612eeef7cd2043b36f20501b6e7585f
Michael Alex Lynch <mlynch5187@gmail.com>
2021-04-06 07:37:11 -0600

    Write game test

---
+++ lottery/test/game_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require "./lottery/lib/contestant"
+require "./lottery/lib/game"
+
+class GameTest < Minitest::Test
+
+  def setup
+    @pick_4 = Game.new('Pick 4', 2)
+    @mega_millions = Game.new('Mega Millions', 5, true)
+  end
+
+  def test_it_exists
+    assert_instance_of Game, @pick_4
+    assert_instance_of Game, @mega_millions
+  end
+
+  def test_it_has_a_name
+    assert_equal "Mega Millions", @mega_millions.name
+  end
+
+  def test_it_has_a_cost
+    assert_equal 5, @mega_millions.cost
+  end
+
+  def test_national_drawing
+    assert_equal true, @mega_millions.national_drawing?
+    assert_equal false, @pick_4.national_drawing?
+  end
+end
