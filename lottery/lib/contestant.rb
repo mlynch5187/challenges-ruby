f8ae5573d25bcdb68369f291690b5ac1d29d7eaa
Michael Alex Lynch <mlynch5187@gmail.com>
2021-04-06 07:48:31 -0600

    Contestants have states and money

--- lottery/lib/contestant.rb
+++ lottery/lib/contestant.rb
@@ -1,7 +1,9 @@
 class Contestant
-  attr_reader :full_name, :age
+  attr_reader :full_name, :age, :state_of_residence, :spending_money
   def initialize(attributes)
     @full_name = attributes[:first_name] + " " + attributes[:last_name]
     @age = attributes[:age]
+    @state_of_residence = attributes[:state_of_residence]
+    @spending_money = attributes[:spending_money]
   end
 end
