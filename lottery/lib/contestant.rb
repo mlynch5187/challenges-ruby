ba76bd53a9904a42f7732e324bfa37e2c8e6ce7c
Michael Alex Lynch <mlynch5187@gmail.com>
2021-04-06 07:47:33 -0600

    Contestants have an age

--- lottery/lib/contestant.rb
+++ lottery/lib/contestant.rb
@@ -1,6 +1,7 @@
 class Contestant
-  attr_reader :full_name
+  attr_reader :full_name, :age
   def initialize(attributes)
     @full_name = attributes[:first_name] + " " + attributes[:last_name]
+    @age = attributes[:age]
   end
 end
